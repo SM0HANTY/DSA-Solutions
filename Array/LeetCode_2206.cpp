8d5974ff392922dedc5f455921607c722349a847
SMohanty <sudhrutiaditi2004@gmail.com>
2025-03-31 21:31:44 +0530

    Create LeetCode_2206.cpp

---
+++ Array/LeetCode_2206.cpp
@@ -0,0 +1,23 @@
+//Divide Array Into Equal Pairs
+
+/*You are given an integer array nums consisting of 2 * n integers.
+
+You need to divide nums into n pairs such that:
+
+Each element belongs to exactly one pair.
+The elements present in a pair are equal.
+Return true if nums can be divided into n pairs, otherwise return false.*/
+
+
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i+=2){
+            if(nums[i]!=nums[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
