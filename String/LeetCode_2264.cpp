31ca31cb21342b683730bd413e10f53fe5c4d412
SMohanty <sudhrutiaditi2004@gmail.com>
2025-08-14 16:08:02 +0530

    Create LeetCode_2264.cpp

---
+++ String/LeetCode_2264.cpp
@@ -0,0 +1,32 @@
+//Largest 3-Same-Digit Number in String
+
+/*You are given a string num representing a large integer. An integer is good if it meets the following conditions:
+
+It is a substring of num with length 3.
+It consists of only one unique digit.
+Return the maximum good integer as a string or an empty string "" if no such integer exists.
+
+Note:
+
+A substring is a contiguous sequence of characters within a string.
+There may be leading zeroes in num or a good integer.*/
+
+
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        string st = "";
+        for (int i = 0; i < num.length() - 2; i++) {
+            if (num[i] == num[i + 1] && num[i] == num[i + 2]) {
+                string temp = "";
+                temp.push_back(num[i]);
+                temp.push_back(num[i]);
+                temp.push_back(num[i]);
+                if (temp > st) {
+                    st = temp;
+                }
+            }
+        }
+        return st;
+    }
+};
