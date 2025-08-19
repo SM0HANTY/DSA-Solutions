7dac4fbeb268cb7e2c0a84ca5b84825e0fb37937
SMohanty <sudhrutiaditi2004@gmail.com>
2025-08-19 12:25:10 +0530

    Create LeetCode_2390.cpp

---
+++ String/LeetCode_2390.cpp
@@ -0,0 +1,33 @@
+//Removing Stars From a String
+
+/*You are given a string s, which contains stars *.
+
+In one operation, you can:
+
+Choose a star in s.
+Remove the closest non-star character to its left, as well as remove the star itself.
+Return the string after all stars have been removed.
+
+Note:
+
+The input will be generated such that the operation is always possible.
+It can be shown that the resulting string will always be unique.
+ */
+
+
+class Solution {
+public:
+    string removeStars(string s) {
+     string ans;
+     for(char i:s){
+        if(i=='*'){
+            if(!ans.empty()){
+                ans.pop_back();
+            }
+        }else{
+            ans.push_back(i);
+        }
+     } 
+     return ans;  
+    }
+};
