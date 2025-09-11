6d9a0c4b07d4541ca8dc36b2c2c977b655602dde
SMohanty <sudhrutiaditi2004@gmail.com>
2025-09-11 14:07:06 +0530

    Create LeetCode_2785.cpp

---
+++ String/LeetCode_2785.cpp
@@ -0,0 +1,29 @@
+//Sort Vowels in a String
+
+/*Given a 0-indexed string s, permute s to get a new string t such that:
+
+All consonants remain in their original places. More formally, if there is an index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
+The vowels must be sorted in the nondecreasing order of their ASCII values. More formally, for pairs of indices i, j with 0 <= i < j < s.length such that s[i] and s[j] are vowels, then t[i] must not have a higher ASCII value than t[j].
+Return the resulting string.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase. Consonants comprise all letters that are not vowels.*/
+
+class Solution {
+public:
+    string sortVowels(string s) {
+        string t;
+       for(int i=0;i<s.length();i++){
+        if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+            t.push_back(s[i]);
+        }
+       }
+       sort(t.begin(),t.end());
+       for(int i=0;i<s.length();i++){
+        if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+            s[i]=t[0];
+            t.erase(0,1);
+        }
+       } 
+       return s;
+    }
+};
