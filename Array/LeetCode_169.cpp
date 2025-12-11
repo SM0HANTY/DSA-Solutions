78cc65a2196ec06ce528947f1d62709a17403795
SMohanty <sudhrutiaditi2004@gmail.com>
2025-12-11 21:35:41 +0530

    Implement second approach for majority element
    Added a second approach to find the majority element using frequency counting.

--- Array/LeetCode_169.cpp
+++ Array/LeetCode_169.cpp
@@ -3,7 +3,7 @@
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.*/
 
 
-
+//1st approach
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -12,3 +12,30 @@ public:
         return nums[n/2];
     }
 };
+
+
+
+
+//2nd approach
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int freq=1;
+        int ans=nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1]){
+                freq++;
+            }else{
+                freq=1;
+                ans=nums[i];
+            }
+        if(freq>n/2){
+            return ans;
+        }
+        }
+        return ans;
+    }
+};
+
