6b7d2a2c1068df734bce931bf229a33417f1682f
SMohanty <sudhrutiaditi2004@gmail.com>
2026-01-18 11:09:45 +0530

    Implement hasSameDigits method in Solution class
    Added a new method to check if a string has the same digits by reducing it iteratively.

--- String/LeetCode_3461.cpp
+++ String/LeetCode_3461.cpp
@@ -34,3 +34,26 @@ public:
       return pal(st); 
     }
 };
+
+
+
+
+//optimal one
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        int i=0;
+        string ans="";
+      while(s.length()>2 && i<s.size()-1){
+             ans+=(s[i]+s[i+1])%10;
+             i++;
+            if(i==s.size()-1){
+                s=ans;
+                i=0;
+                ans="";
+            }
+      } 
+      if(s.size()==2 && s[0]==s[1])return true;
+      return false;
+    }
+};
